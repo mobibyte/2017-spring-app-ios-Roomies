0c30c86f5c75a55bdb9bedd626f0e64e68a9517b
maryehuerta <mary.huerta@mavs.uta.edu>
2016-11-11 18:34:19 -0600

    Got rid of blue

--- Roomies/ExpensesTableViewController.swift
+++ Roomies/ExpensesTableViewController.swift
@@ -29,12 +29,12 @@ class ExpensesTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 2
     }
 
     /*
