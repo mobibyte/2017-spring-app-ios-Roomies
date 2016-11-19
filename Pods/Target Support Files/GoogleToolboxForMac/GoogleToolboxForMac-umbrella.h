bf64bb89884347d3776ec61d9bdf85a8cebcf4e9
Cameron Moreau <cameron.cm6@gmail.com>
2016-11-18 18:56:54 -0600

    Setup with firebase

--- Pods/Target Support Files/GoogleToolboxForMac/GoogleToolboxForMac-umbrella.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "GTMDefines.h"
-#import "GTMNSData+zlib.h"
-
-FOUNDATION_EXPORT double GoogleToolboxForMacVersionNumber;
-FOUNDATION_EXPORT const unsigned char GoogleToolboxForMacVersionString[];
-
