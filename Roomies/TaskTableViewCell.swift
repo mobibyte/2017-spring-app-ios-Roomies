70795aebb4a8ab473ec30d599345e3cf455f6a8f
maryehuerta <mary.huerta@mavs.uta.edu>
2017-01-27 17:02:20 -0600

    Tasks TVC

---
+++ Roomies/TaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskTableViewCell.swift
+//  Roomies
+//
+//  Created by Mary Huerta on 1/22/17.
+//  Copyright © 2017 Mobi. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
